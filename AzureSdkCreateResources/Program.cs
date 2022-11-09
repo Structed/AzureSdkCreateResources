07077337e75a84a7fd9238320b29dac6925531d1
Johannes Ebner <Structed@users.noreply.github.com>
2022-11-09 10:12:34 +0100

    Improved code

--- AzureSdkCreateResources/Program.cs
+++ AzureSdkCreateResources/Program.cs
@@ -10,32 +10,35 @@ using Azure.ResourceManager.Network;
 using Azure.ResourceManager.Network.Models;
 using Azure.ResourceManager.Resources;
 
-var accountId = Guid.NewGuid().ToString();
-
-var tenantId = Environment.GetEnvironmentVariable("AZ_ROLE_TENANT_ID", EnvironmentVariableTarget.Process) ?? throw new ArgumentNullException("Environment.GetEnvironmentVariable(\"AZ_ROLE_TENANT_ID\", EnvironmentVariableTarget.Process)");
-var clientId = Environment.GetEnvironmentVariable("AZ_ROLE_CLIENT_ID", EnvironmentVariableTarget.Process) ?? throw new ArgumentNullException("Environment.GetEnvironmentVariable(\"AZ_ROLE_CLIENT_ID\", EnvironmentVariableTarget.Process)");
-var clientSecret = Environment.GetEnvironmentVariable("AZ_ROLE_CLIENT_SECRET", EnvironmentVariableTarget.Process) ?? throw new ArgumentNullException("Environment.GetEnvironmentVariable(\"AZ_ROLE_CLIENT_SECRET\", EnvironmentVariableTarget.Process)");
-
-// Authenticate the arm client
-var credentials = new ClientSecretCredential(
-    System.Environment.GetEnvironmentVariable("AZ_ROLE_TENANT_ID", EnvironmentVariableTarget.Process),
-    System.Environment.GetEnvironmentVariable("AZ_ROLE_CLIENT_ID", EnvironmentVariableTarget.Process),
-    System.Environment.GetEnvironmentVariable("AZ_ROLE_CLIENT_SECRET", EnvironmentVariableTarget.Process)
-);
-ArmClient armClient = new ArmClient(credentials);
-SubscriptionResource subscription = await armClient.GetDefaultSubscriptionAsync();
-var resourceGroupResponse = await subscription.GetResourceGroupAsync("sdk-test");
-var resourceGroup = resourceGroupResponse.Value;
-
+// CONFIG STUFF
+const string resourceNamePrefix = "sdkt";   // This Resource Group has to exist before executing, including proper permissions for the Service Principal to create resources in this Resource Group
+const string baseResourceGroup = "sdk-test";
+const string subnetName = $"{resourceNamePrefix}-subnet";
+const string vmSku = "Standard_NC4as_T4_v3";    // Make sure you have quota! https://learn.microsoft.com/en-us/azure/azure-resource-manager/management/azure-subscription-service-limits
+string vmAdminPassword = Guid.NewGuid().ToString();
+string accountId = Guid.NewGuid().ToString();
 AzureLocation location = AzureLocation.WestEurope;
 
 
 
 
-// NetworkSecurityGroupCollection nsgCollection = resourceGroup.GetNetworkSecurityGroups();
-// NetworkSecurityGroupData nsgData = new NetworkSecurityGroupData();
-// nsgData.Location = location;
-// var newNetworkSecurityGroup = await nsgCollection.CreateOrUpdateAsync(Azure.WaitUntil.Started, $"rpb-nsg-{accountId}", nsgData);
+var tenantId = Environment.GetEnvironmentVariable("AZ_ROLE_TENANT_ID", EnvironmentVariableTarget.Process) ?? throw new ArgumentNullException("Environment.GetEnvironmentVariable(\"AZ_ROLE_TENANT_ID\", EnvironmentVariableTarget.Process)");
+var clientId = Environment.GetEnvironmentVariable("AZ_ROLE_CLIENT_ID", EnvironmentVariableTarget.Process) ?? throw new ArgumentNullException("Environment.GetEnvironmentVariable(\"AZ_ROLE_CLIENT_ID\", EnvironmentVariableTarget.Process)");
+var clientSecret = Environment.GetEnvironmentVariable("AZ_ROLE_CLIENT_SECRET", EnvironmentVariableTarget.Process) ?? throw new ArgumentNullException("Environment.GetEnvironmentVariable(\"AZ_ROLE_CLIENT_SECRET\", EnvironmentVariableTarget.Process)");
+
+// Authenticate the ARM client
+var credentials = new ClientSecretCredential(
+    tenantId,
+    clientId,
+    clientSecret
+);
+
+ArmClient armClient = new ArmClient(credentials);
+SubscriptionResource subscription = await armClient.GetDefaultSubscriptionAsync();
+var resourceGroupResponse = await subscription.GetResourceGroupAsync(baseResourceGroup);
+var resourceGroup = resourceGroupResponse.Value;
+
+
 
 // Create network interface & attach existing IP address for account
 NetworkInterfaceCollection niCollection = resourceGroup.GetNetworkInterfaces();
@@ -53,24 +56,23 @@ publicIpAddressData.Sku = new PublicIPAddressSku
 };
 publicIpAddressData.Location = location;
 publicIpAddressData.PublicIPAllocationMethod = NetworkIPAllocationMethod.Static;
-var pipResponse = await ipCollection.CreateOrUpdateAsync(WaitUntil.Completed, $"remote-performances-pip-{accountId}", publicIpAddressData);
+var pipResponse = await ipCollection.CreateOrUpdateAsync(WaitUntil.Completed, $"{resourceNamePrefix}-pip-{accountId}", publicIpAddressData);
 NetworkInterfaceIPConfigurationData ipData = new NetworkInterfaceIPConfigurationData();
 ipData.Primary = true;
 ipData.Id = pipResponse.Value.Id;
 niData.IPConfigurations.Add(ipData);
 
-var newNetworkInterface = await CreateVnet(resourceGroup, accountId, pipResponse.Value);
-// var newNetworkInterface = await niCollection.CreateOrUpdateAsync(Azure.WaitUntil.Started, $"rpb-network-interface-{accountId}", niData);
+var newNetworkInterface = await CreateVnet(resourceGroup, accountId, pipResponse.Value, subnetName);
 Console.WriteLine($"NIC with ID {newNetworkInterface.Id} created");
 
 
 // Create Virtual Machine
-await CreateVm(resourceGroup, location, accountId, newNetworkInterface);
+var vmCreationResult = await CreateVm(resourceGroup, location, accountId, newNetworkInterface, vmSku, vmAdminPassword);
+Console.WriteLine($"VM {vmCreationResult.Value.Data.Name} created");
 
 
-async Task<NetworkInterfaceResource> CreateVnet(ResourceGroupResource resourceGroupResource, string playerId, PublicIPAddressResource publicIpAddress)
+async Task<NetworkInterfaceResource> CreateVnet(ResourceGroupResource resourceGroupResource, string playerId, PublicIPAddressResource publicIpAddress, string subnetName)
 {
-    var subnetName = "mySubnet";
     var vnetData = new VirtualNetworkData
     {            Location = resourceGroupResource.Data.Location,
 
@@ -86,10 +88,7 @@ async Task<NetworkInterfaceResource> CreateVnet(ResourceGroupResource resourceGr
     };
 
     var virtualNetworkContainer = resourceGroupResource.GetVirtualNetworks();
-    var vnet = await virtualNetworkContainer.CreateOrUpdate(WaitUntil.Completed, $"rpb-vnet-{playerId}", vnetData).WaitForCompletionAsync();
-    // Console.WriteLine($"Created a Virtual Network {vnet.Value.Id}");
-
-    // Console.WriteLine("Creating a Network Interface");
+    var vnet = await virtualNetworkContainer.CreateOrUpdate(WaitUntil.Completed, $"{resourceNamePrefix}-vnet-{playerId}", vnetData).WaitForCompletionAsync();
     var networkInterfaceData = new NetworkInterfaceData()
     {
         Location = resourceGroupResource.Data.Location,
@@ -99,27 +98,25 @@ async Task<NetworkInterfaceResource> CreateVnet(ResourceGroupResource resourceGr
                 Name = "Primary",
                 Primary = true,
                 Subnet = new SubnetData() { Id = (await vnet.Value.GetSubnetAsync(subnetName)).Value.Id },
-                // PrivateIPAllocationMethod = NetworkIPAllocationMethod.Static,
                 PublicIPAddress = new PublicIPAddressData() { Id = publicIpAddress.Id }
             }
         }
     };
 
     var networkInterfaceContainer = resourceGroupResource.GetNetworkInterfaces();
-    var networkInterface = await (await networkInterfaceContainer.CreateOrUpdateAsync(WaitUntil.Completed, $"rpb-nic-{playerId}", networkInterfaceData)).WaitForCompletionAsync();
+    var networkInterface = await (await networkInterfaceContainer.CreateOrUpdateAsync(WaitUntil.Completed, $"{resourceNamePrefix}-nic-{playerId}", networkInterfaceData)).WaitForCompletionAsync();
     return networkInterface.Value;
 }
 
 
 
-async Task CreateVm(ResourceGroupResource resourceGroupResource, AzureLocation azureLocation, string playerId,
-    NetworkInterfaceResource networkInterfaceResource)
+async Task<ArmOperation<VirtualMachineResource>> CreateVm(ResourceGroupResource resourceGroupResource, AzureLocation azureLocation, string playerId, NetworkInterfaceResource networkInterfaceResource, string sku, string password)
 {
     VirtualMachineCollection vmCollection = resourceGroupResource.GetVirtualMachines();
     VirtualMachineData vmData = new VirtualMachineData(azureLocation);
     vmData.Tags.Add("id", playerId);
     vmData.HardwareProfile = new VirtualMachineHardwareProfile();
-    vmData.HardwareProfile.VmSize = new VirtualMachineSizeType("Standard_NC4as_T4_v3");
+    vmData.HardwareProfile.VmSize = new VirtualMachineSizeType(sku);
     vmData.StorageProfile = new VirtualMachineStorageProfile();
     vmData.StorageProfile.OSDisk = new VirtualMachineOSDisk("FromImage");
     vmData.StorageProfile.ImageReference = new ImageReference();
@@ -127,19 +124,20 @@ async Task CreateVm(ResourceGroupResource resourceGroupResource, AzureLocation a
     vmData.StorageProfile.ImageReference.Publisher = "MicrosoftWindowsServer";
     vmData.StorageProfile.ImageReference.Sku = "2022-Datacenter";
     vmData.StorageProfile.ImageReference.Version = "latest";
-    vmData.StorageProfile.OSDisk.Name = $"Venue-RPB-OsDisk-{playerId}";
+    vmData.StorageProfile.OSDisk.Name = $"{resourceNamePrefix}-OsDisk-{playerId}";
     vmData.StorageProfile.OSDisk.DiffDiskSettings = new DiffDiskSettings();
     vmData.StorageProfile.OSDisk.DiffDiskSettings.Option = "Local";
     vmData.StorageProfile.OSDisk.DiffDiskSettings.Placement = "ResourceDisk";
     vmData.StorageProfile.OSDisk.Caching = CachingType.ReadOnly;
     vmData.OSProfile = new VirtualMachineOSProfile();
-    vmData.OSProfile.AdminUsername = "joebner";
-    vmData.OSProfile.AdminPassword = Guid.NewGuid().ToString();
-    vmData.OSProfile.ComputerName = "RPB";
+    vmData.OSProfile.AdminUsername = "superuser";
+    vmData.OSProfile.AdminPassword = password;
+    vmData.OSProfile.ComputerName = $"{resourceNamePrefix}vm";
     vmData.NetworkProfile = new VirtualMachineNetworkProfile();
     VirtualMachineNetworkInterfaceReference ni = new VirtualMachineNetworkInterfaceReference();
     ni.Id = new ResourceIdentifier(networkInterfaceResource.Id);
     vmData.NetworkProfile.NetworkInterfaces.Add(ni);
     vmData.UserData = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(playerId));
-    var newVM = await vmCollection.CreateOrUpdateAsync(Azure.WaitUntil.Started, $"Venue-RPB-{playerId}", vmData);
+    var newVm = await vmCollection.CreateOrUpdateAsync(Azure.WaitUntil.Completed, $"{resourceNamePrefix}-vm-{playerId}", vmData);
+    return newVm;
 }
